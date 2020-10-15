73b6836ef20f55a645b652a47366630ac16cd4d7
JuniorDeveloper54 <lotifurshabbir54@gmail.com>
2020-10-15 18:22:05 +0600

    client site done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import UsersComponent from './Components/UsersComponent/UsersComponent';
+import AdminsComponents from './Components/AdminsComponent/AdminsComponents';
+import NoMatch from './Components/NoMatch/NoMatch';
+import Login from './Components/Login/Login';
+import { createContext } from 'react';
+import { useState } from 'react';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext();
+
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <PrivateRoute path="/usersComponent/:project">
+            <UsersComponent />
+          </PrivateRoute>
+          <PrivateRoute path="/adminsComponents">
+            <AdminsComponents />
+          </PrivateRoute>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
